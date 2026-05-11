88953a175ec77ce6a847896b1642c820421cf413
DeepanshuRathore23 <deepanshurathore2004@gmail.com>
2026-05-11 13:21:53 +0530

    database seeded successfully

--- lib/seed.ts
+++ lib/seed.ts
@@ -8,12 +8,8 @@ if (!process.env.POSTGRES_URL) {
 }
 const sql = postgres(process.env.POSTGRES_URL, { ssl: "require" });
 
-/* ------------------ HELPERS ------------------ */
-const formatDate = (dateStr: string)  => {
-  const [day, month, year] = dateStr.split("-");
-  return `${year}-${month}-${day}`; // YYYY-MM-DD
-};
 
+//employees table seeded successfully
 async function seedEmployees() {
   await sql`DROP TABLE IF EXISTS employees`
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -23,19 +19,23 @@ async function seedEmployees() {
       name VARCHAR(100),
       email VARCHAR(100) UNIQUE,
       password VARCHAR(255),      
-      team_id VARCHAR(255),
+      team_id uuid,
       team_name VARCHAR(255),
-      position VARCHAR(255) CHECK(position IN('Senior Software Engineer', 'Frontend Engineer', 'QA Engineer', 'Backend Engineer', 'Tester', 'Devops Engineer')),
-      role VARCHAR(10) CHECK(role IN('admin', 'team_lead', 'team_member', 'on_bench'))
+      position VARCHAR(255) CHECK(position IN('Admin', 'Senior Software Engineer', 'Frontend Engineer', 'QA Engineer', 'Backend Engineer', 'Tester', 'Devops Engineer')),
+      role VARCHAR(255) CHECK(role IN('admin', 'team_lead', 'team_member', 'on_bench')),
       FOREIGN KEY (team_id) REFERENCES teams(id)
     );
   `;
 
   for (const emp of employees) {
     const encryptedPassword = await bcrypt.hash(emp.password, 10);
-    const team_rows = await sql`SELECT id FROM teams WHERE name = ${emp.team_name}`;
-    const teamId = team_rows[0]?.id;
 
+    let teamId = null;
+    if(emp.team_name) {
+      const team_rows = await sql`SELECT id FROM teams WHERE name = ${emp.team_name}`;
+      teamId = team_rows[0]?.id  || null;
+    }
+    
     await sql`
       INSERT INTO employees (
         name, email, password, team_id, team_name,
@@ -50,13 +50,14 @@ async function seedEmployees() {
   }
 }
 
+//teams table seeded successfully
 async function seedTeams() {
   await sql`DROP TABLE IF EXISTS teams`
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
     CREATE TABLE IF NOT EXISTS teams (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(100),
+      name VARCHAR(100) UNIQUE,
       open_tasks VARCHAR(255),
       completed_tasks VARCHAR(255)
     );
@@ -67,7 +68,7 @@ async function seedTeams() {
       INSERT INTO teams (
         name, open_tasks, completed_tasks
       )
-      VALUES
+      VALUES(
         ${team.name}, ${team.open_tasks}, ${team.completed_tasks}
       )
       ON CONFLICT (name) DO NOTHING;
@@ -75,58 +76,57 @@ async function seedTeams() {
   }
 }
 
+//projects table seeded successfully
 async function seedProjects() {
   await sql`DROP TABLE IF EXISTS projects`
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
     CREATE TABLE IF NOT EXISTS projects (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(100),
+      name VARCHAR(100) UNIQUE,
       description VARCHAR(255),
-      team_id VARCHAR(100),
+      team_id uuid,
       team_name VARCHAR(255),
       status VARCHAR(100),
-      team_lead VARCHAR(100),
       date_of_creation DATE,
       date_of_assigning DATE,
       deadline DATE,
       progress VARCHAR(100),
-      tasks TEXT[],
       activity_logs TEXT[],
       FOREIGN KEY (team_id) REFERENCES teams(id)
     );
   `;
 
   for (const prj of projects) {
-    const team_rows = await sql`SELECT id, lead FROM teams WHERE name = ${prj.team_name}`;
+    const team_rows = await sql`SELECT id FROM teams WHERE name = ${prj.team_name}`;
     const teamId=  team_rows[0]?.id;
-    const teamLead=  team_rows[0]?.lead;
 
     await sql`
       INSERT INTO projects (
-        name, description, team_id, team_name, status, team_lead, date_of_creation, date_of_assigning, deadline, progress,
-        tasks, activity_logs
+        name, description, team_id, team_name, status, date_of_creation, date_of_assigning, deadline, progress,
+        activity_logs
       )
       VALUES (
-        ${prj.name}, ${prj.description}, ${teamId}, ${prj.team_name}, ${prj.status}, ${teamLead}, 
-        ${prj.date_of_creation}, ${prj.date_of_assigning}, ${prj.deadline}, ${prj.progress}, ${prj.tasks}, ${prj.activity_logs}
+        ${prj.name}, ${prj.description}, ${teamId}, ${prj.team_name}, ${prj.status}, 
+        ${prj.date_of_creation}, ${prj.date_of_assigning}, ${prj.deadline}, ${prj.progress}, ${prj.activity_logs}
       )
       ON CONFLICT (name) DO NOTHING;
     `;
   }
 }
 
+//tasks table seeded successfully
 async function seedTasks() {
   await sql`DROP TABLE IF EXISTS tasks`
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await sql`
     CREATE TABLE IF NOT EXISTS tasks (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      name VARCHAR(100),
+      name VARCHAR(100) UNIQUE,
       description VARCHAR(255),
-      project_id VARCHAR(100),
+      project_id uuid,
       project_name VARCHAR(255),
-      assigned_emp_id VARCHAR(100),
+      assigned_emp_id uuid,
       assigned_emp_email VARCHAR(100),
       date_of_creation DATE,
       date_of_assigning DATE,
@@ -140,22 +140,29 @@ async function seedTasks() {
   `;
 
   for (const tsk of tasks) {
-    const emp_rows = await sql`SELECT id, team_id FROM employees WHERE email = ${tsk.assigned_emp_email}`;
-    const empId=  emp_rows[0]?.id;
-    const teamId = emp_rows[0]?.team_id;
+    let empId = null;
+    let projectId = null;
+    let projectName = null;
 
-    const project_rows = await sql`SELECT id, name FROM projects WHERE team_id = ${teamId}`
-    const projectId=  project_rows[0]?.id;
-    const prjectName = project_rows[0]?.name;
+    if(tsk.assigned_emp_email) {
+      const emp_rows = await sql`SELECT id, team_id FROM employees WHERE email = ${tsk.assigned_emp_email}`;
+      empId = emp_rows[0]?.id;
+      const teamId = emp_rows[0]?.team_id;
+
+      const project_rows = await sql`SELECT id, name FROM projects WHERE team_id = ${teamId}`
+      projectId=  project_rows[0]?.id;
+      projectName = project_rows[0]?.name;
+    }
+    
 
     await sql`
-      INSERT INTO projects (
+      INSERT INTO tasks (
         name, description, project_id, project_name, assigned_emp_id, assigned_emp_email, 
-        status, date_of_creation, date_of_assigning, deadline, priority, activity_logs
+        date_of_creation, date_of_assigning, deadline, status, priority, activity_logs
       )
       VALUES (
-        ${tsk.name}, ${tsk.description}, ${projectId}, ${prjectName}, ${empId}, ${tsk.assigned_emp_email}, 
-        ${tsk.status}, ${tsk.date_of_creation}, ${tsk.date_of_assigning}, ${tsk.deadline}, ${tsk.priority}, ${tsk.activity_logs}
+        ${tsk.name}, ${tsk.description}, ${projectId}, ${projectName}, ${empId}, ${tsk.assigned_emp_email}, 
+        ${tsk.date_of_creation}, ${tsk.date_of_assigning}, ${tsk.deadline}, ${tsk.status}, ${tsk.priority}, ${tsk.activity_logs}
       )
       ON CONFLICT (name) DO NOTHING;
     `;
@@ -164,10 +171,10 @@ async function seedTasks() {
 
 export async function seedDatabase(){
     try{
-        console.log("🌱 Seeding database...");
+        console.log("🌱 Nothing to seed...");
 
-
-        console.log("✅ Seeding completed!");
+        // await seedTasks();
+        // console.log("✅ Seeding completed!");
     } catch(error) {
         console.error("❌ Seeding failed:", error);
         throw error;
